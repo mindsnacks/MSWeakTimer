3dd92454c6fb6407ee5bad99658efa64b567d957
Javier Soto <javier.api@gmail.com>
2013-07-12 15:15:51 -0700

    AppleDoc friendly header comments!

--- MSWeakTimer.h
+++ MSWeakTimer.h
@@ -8,17 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ `MSWeakTimer` behaves similar to an `NSTimer` but doesn't retain the target.
+ This timer is implemented using GCD, so you can schedule and unschedule it on arbitrary queues (unlike regular NSTimers!)
+ */
 @interface MSWeakTimer : NSObject
 
 /**
- * @class `MSWeakTimer` behaves similar to an `NSTimer` but doesn't retain the target.
- * @discussion this timer is implemented using GCD, so you can schedule and unschedule it on arbitrary queues
- * (unlike regular NSTimers!)
- * It's safe to retain this timer by the object that is also the target.
- * You can call -invalidate from any queue, doesn't have to be the queue from where you scheduled it
- * or the provided dispatchQueue.
- * @param dispatchQueue the queue where the delegate method will be dispatched. It can be either a serial or concurrent queue. Note: the queue is retained.
- * @see `-invalidate`.
+ * Creates an `MSWeakTimer` object and schedules it to start ticking inmediately.
+ * @note It's safe to retain the returned timer by the object that is also the target.
+ * or the provided `dispatchQueue`.
+ * @param timeInterval how frequently `selector` will be invoked on `target`. If the timer doens't repeat, it will only be invoked once, approximately `timeInterval` seconds from the time you call this method.
+ * @param repeats if `YES`, `selector` will be invoked on `target` until the `MSWeakTimer` object is deallocated or until you call `invalidate`. If `NO`, it will only be invoked once.
+ * @param dispatchQueue the queue where the delegate method will be dispatched. It can be either a serial or concurrent queue.
+ * @see `invalidate`.
  */
 + (MSWeakTimer *)scheduledTimerWithTimeInterval:(NSTimeInterval)timeInterval
                                          target:(id)target
@@ -28,18 +31,20 @@
                                   dispatchQueue:(dispatch_queue_t)dispatchQueue;
 
 /**
- * @discussion causes the timer to be fired asynchronously on the provided dispatchQueue.
+ * Causes the timer to be fired synchronously manually on the queue from which you call this method.
  * You can use this method to fire a repeating timer without interrupting its regular firing schedule.
  * If the timer is non-repeating, it is automatically invalidated after firing, even if its scheduled fire date has not arrived.
  */
 - (void)fire;
 
 /**
- * @discussion you should call this method on repeatable timers in order to stop it from running and trying
- * to perform call the delegate method. You can simply do it in the -dealloc method of the delegate.
- * (which usually is the object that owns the timer).
- * Since it doesn't retain the delegate, unlike a regular NSTimer, your -dealloc method will actually be called
- * and it's easier to place the -invalidate call there, instead of figuring out a safe place to do it.
+ * You can call this method on repeatable timers in order to stop it from running and trying
+ * to call the delegate method.
+ * You can call this method from any queue, it doesn't have to be the queue from where you scheduled it.
+ * @note If the object that owns the `MSWeakTimer` is going to be deallocated, you can simply release the `MSWeakTimer`,
+ * which will cause it to deallocate, essentially invalidating it, so you don't need to explicitely call this method in that case.
+ * Since it doesn't retain the delegate, unlike a regular `NSTimer`, your `dealloc` method will actually be called
+ * and it's easier to place the `invalidate` call there, instead of figuring out a safe place to do it.
  */
 - (void)invalidate;
 
